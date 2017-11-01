ad4e59cc9ad00b09fa730f227da1cfe69d416dff
Guihrmsfo <guilherme_oliveira55@hotmail.com>
2017-11-01 16:43:26 -0200

    codeclimate issues are fixed

--- app/controllers/checkin_controller.rb
+++ app/controllers/checkin_controller.rb
@@ -2,9 +2,9 @@ class CheckinController < SecureController
   def checkin
     @checkin = Checkin.new(user_id: current_user.id, experience_id: params['experience_id'])
     if @checkin.save
-      render status: 200, :json => { success: true }
+      render status: 200, json: { success: true }
     else
-      render status: 500, :json => { success: false }
+      render status: 500, json: { success: false }
     end
   end
 end
