58aea69c76742054e62c53efc55913da98f763e4
Guihrmsfo <guilherme_oliveira55@hotmail.com>
2017-11-01 22:28:27 -0200

    fixed codeclimate issues

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,7 +2,7 @@ class Experience < ApplicationRecord
   has_many :checkin
   attr_accessor :current_user
   # has user visited experience locale?
-  def visited( user=nil )
+  def visited(user = nil)
     user ||= current_user
     Checkin.where(user_id: user.id, experience_id: id).first.present?
   end
